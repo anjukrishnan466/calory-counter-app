ebcae3e7a30dcdc5d38fe378b193a34e0f69aa61
Anju Krishnan <anjukrishnan@focaloid.com>
2023-01-14 11:12:13 +0530

    commit

--- Calorie_Counter_App/adminuser/serializers.py
+++ Calorie_Counter_App/adminuser/serializers.py
@@ -29,7 +29,32 @@ class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
         fields = ["name", "calory_per_hr"]
+# class  UserSerializer(serializers.ModelSerializer):
+    
+#     class Meta:
+#         model = User
+#         fields = ["id", "username",'email']
+        
+#         # Register Serializer
+# class RegisterSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('id', 'username', 'email', 'password')
+#         extra_kwargs = {'password': {'write_only': True}}
 
+#     def create(self, validated_data):
+#         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+#         return user
+# class LoginSerializer(serializers.Serializer):
+#     username = serializers.CharField()
+#     password = serializers.CharField()
+
+#     def validate(self, data):
+#         user = authenticate(**data)
+#         if user and user.is_active:
+#             return user
+#         raise serializers.ValidationError('Incorrect Credentials Passed.')
+    
     
 class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
--- Calorie_Counter_App/user/urls.py
+++ Calorie_Counter_App/user/urls.py
@@ -3,18 +3,18 @@
 from django.urls import path
 from .import views
 from knox import views as knox_views
-from .views import RegistrationAPI
+from .views import RegistrationAPI,LoginAPI
 from knox import views as knox_views
 urlpatterns = [
 # path('register',RegisterAPI.as_view(), name="register"),
 # path('login', LoginAPI.as_view(), name='login'),
 path('logout', knox_views.LogoutView.as_view(), name='logout'),
-
+path('login/', LoginAPI.as_view()),
 path('recordmeal/', views.RecordMeal.as_view()),
 path('calory_status', views.calory_status, name="calory_status"),
 
 path('food_store_by_user', views.food_store_by_user, name="food_store_by_user"),
 path('activity_store_by_user', views.activity_store_by_user, name="activity_store_by_user"),
-    path('register/', RegistrationAPI.as_view()),
+path('register/', RegistrationAPI.as_view()),
 
 ]
